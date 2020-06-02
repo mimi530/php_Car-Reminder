221d2142b6baa82723da4d000fc7dd0c003c80dc
Michał Domżalski <michal.domzalski99@wp.pl>
2020-06-03 01:14:23 +0200

    synchro działa!

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta author="Michał Domżalski">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ćwiczenia PHP</title>
+</head>
+<body>
+    <?php echo "It's working!"; ?>
+</body>
+</html>
