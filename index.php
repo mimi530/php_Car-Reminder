d35a1e0d67f63be2497d32da414f3f54326a2f5a
Michał Domżalski <michal.domzalski99@wp.pl>
2020-06-04 06:25:31 +0200

    rejestracja prawie gotowa

--- index.php
+++ index.php
@@ -1,6 +1,84 @@
 <?php
-    #code ^^
-    
+    #formluarz rejestracji
+    session_start();
+    $bledy = [];
+    define('WYMAGANE','To pole jest wymagane!');
+    $login = '';
+    $email = '';
+    $haslo = '';
+    $haslo2 = '';
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        #zabezpiecznie danych
+        $login = dane_post('login');
+        $email = dane_post('email');
+        $haslo = dane_post('haslo');
+        $haslo2 = dane_post('haslo2');
+        #walidacja warunków rejestracji
+        if(!$login) {
+            $bledy['login'] = WYMAGANE;
+        } elseif (strlen($login)<3 || strlen($login)>20) {
+            $bledy['login'] = 'Login musi posiadać od 3-20 znaków!';
+        }
+        if(!$email) {
+            $bledy['email'] = WYMAGANE;
+        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $bledy['email'] = 'E-mail musi być poprawny!';
+        }
+        if(!$haslo) {
+            $bledy['haslo'] = WYMAGANE;
+        } elseif(strlen($haslo)<8) {
+            $bledy['haslo'] = "Hasło za krótkie!";
+        }
+        if(!$haslo2) {
+            $bledy['haslo2'] = WYMAGANE;
+        }
+        if($haslo && $haslo2 && strcmp($haslo, $haslo2)) {
+            $bledy['haslo2'] = 'Hasła muszą się zgadzać!';
+        }
+        #walidacja skonczona
+        if(empty($bledy)) {
+            require_once('connect.php');
+            mysqli_report(MYSQLI_REPORT_STRICT);
+            try {
+                $conn = new mysqli($host, $db_user, $db_pass, $db_name);
+                if($conn->connect_errno!=0) {
+                    throw new Exception(mysqli_connect_errno());
+                }
+                else {
+                    #czy email istnieje juz w bazie
+                    $wynik = $conn->query("SELECT id FROM users WHERE email='$email'");
+                    if(!$wynik) throw new Exception($conn->error);
+                    $ile_maili = $wynik->num_rows;
+                    if($ile_maili) {
+                        $bledy['email'] = 'Taki email istnieje już w bazie danych!';
+                    }
+                    #czy login istnieje juz w bazie
+                    $wynik = $conn->query("SELECT id FROM users WHERE login='$login'");
+                    if(!$wynik) throw new Exception($conn->error);
+                    $ile_loginow = $wynik->num_rows;
+                    if($ile_loginow) {
+                        $bledy['login'] = 'Taki login istnieje już w bazie danych!';
+                    }
+                    if(empty($bledy)) {
+                        if($conn->query("INSERT INTO users VALUES (NULL, '$login', '$haslo', '$email')")) {
+                            $_SESSION['udana'] = ' udana!';
+                            header('Location: index.php');
+                        }
+                        else throw new Exception($conn->error);
+                    }
+                    $conn->close();
+                }
+            }
+            catch(Exception $error) {
+                echo 'Błąd serwera :/';
+            }
+        }
+    }
+    function dane_post($pole)
+    {
+        $_POST[$pole] ??= '';
+        return htmlspecialchars(stripslashes($_POST[$pole]));
+    }
 ?>
 <!DOCTYPE html>
 <html lang="pl">
@@ -8,7 +86,6 @@
     <meta charset="UTF-8">
     <meta author="Michał Domżalski">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" 
     href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" 
     integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
@@ -35,20 +112,26 @@
     <h1>Car-Reminder!</h1>
     <h4>Strona stworzona w celu przećwiczenia umiejętności PHP. Żeby nie była bezużyteczna to symuluje 
         "przypominacz samochodowy" aby pamiętać kiedy przeprowadzaliśmy okresowe naprawy :)</h4></br>
-    <h2>Rejestracja</h2>
-    <form action="rejestracja.php" method="post">
+    <h2>Rejestracja<?php echo $_SESSION['udana'] ??= '' ?></h2>
+    <form action="" method="post">
         <div class="row">
             <div class="col">
                 <div class="form-group">
                     <label>Login</label>
-                    <input class="form-control" type="text" name="username">
+                    <input class="form-control <?php echo isset($bledy['login']) ? 'is-invalid' : ''?>" type="text" name="login" value="<?php echo $login;?>">
                     <small class="form-text text-muted">3-20 znaków.</small>
+                    <div class="invalid-feedback">
+                        <?php echo $bledy['login'] ?>
+                    </div>
                 </div>
             </div>
             <div class="col">
                 <div class="form-group">
                     <label>E-mail</label>
-                    <input class="form-control" type="text" name="email">
+                    <input class="form-control <?php echo isset($bledy['email']) ? 'is-invalid' : ''?>" type="text" name="email" value="<?php echo $email;?>">
+                    <div class="invalid-feedback">
+                        <?php echo $bledy['email'] ?>
+                    </div>
                 </div>
             </div>
         </div>
@@ -56,19 +139,25 @@
             <div class="col">
                 <div class="form-group">
                     <label>Hasło</label>
-                    <input class="form-control" name="haslo" type="password">
+                    <input class="form-control <?php echo isset($bledy['haslo']) ? 'is-invalid' : ''?>" name="haslo" type="password" value="<?php echo $haslo;?>">
                     <small class="form-text text-muted">Min. 8 znaków</small>
+                    <div class="invalid-feedback">
+                        <?php echo $bledy['haslo'] ?>
+                    </div>
                 </div>
             </div>
             <div class="col">
                 <div class="form-group">
                     <label>Powtórz hasło</label>
-                    <input class="form-control" name="haslo2" type="password" >
+                    <input class="form-control <?php echo isset($bledy['haslo2']) ? 'is-invalid' : ''?>" name="haslo2" type="password" value="<?php echo $haslo2;?>">
+                    <div class="invalid-feedback">
+                        <?php echo $bledy['haslo2'] ?>
+                    </div>
                 </div>
             </div>
         </div>
         <div class="form-group">
-            <button class="btn btn-primary">Zarejestruj!</button>
+            <input type="submit" class="btn btn-primary" value="Zarejestruj!">
         </div>
     </form>
     </br>
@@ -94,3 +183,4 @@
     </form> 
 </body>
 </html>
+<?php unset($_SESSION['udana']) ?>
